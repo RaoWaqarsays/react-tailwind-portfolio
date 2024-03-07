460f4a39f1d71ca2f61f94a00cd05a64b17dab93
RaoWaqarsays <wikirao47@gmail.com>
2024-03-07 12:03:36 +0500

    Update dependencies

--- pages/index.js
+++ pages/index.js
@@ -56,12 +56,14 @@ export default function Home() {
               Rao Waqar
             </h2>
             <h3 className="text-2xl py-2 dark:text-white md:text-3xl">
-              Blockchain Developer.
+              Blockchain Developer
             </h3>
             <p className="text-md py-5 leading-8 text-gray-800 dark:text-gray-200 max-w-xl mx-auto md:text-xl">
-              Recent Graduate COMSATS University | Aspiring Web3 Developer |
-              Koinos Blockchain Enthusiast |<br />
-              Seeking Internship Opportunities!
+              passionate
+              <span className="text-teal-500"> Blockchain Developer, </span>
+              enthusiastic about the Blockchain and web3 industry, looking
+              forward to learn something new everyday and contribute to the
+              industry!
             </p>
             <div className="text-5xl flex justify-center gap-16 py-3 text-gray-600 dark:text-gray-400">
               <a
